f8db44b72b948b40848550e0bc308e7ff9e4574a
Rannael Santiago <74615002+rannaelsant@users.noreply.github.com>
2022-12-05 23:41:38 -0300

    Add files via upload

---
+++ paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(355, 0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+    def move(self):
+        self.listen()
+        self.onkey(go_up(), "Up")
+        self.onkey(go_down, "Down")
